c3933a2c0542b98de0f27d38deff18bc8b503412
humangas <humangas.net@gmail.com>
2017-11-23 05:05:17 +0900

    Fixed typo, removed debug code

--- roles/vim/setup.sh
+++ roles/vim/setup.sh
@@ -22,15 +22,12 @@ config() {
 
 install() {
 	_install_deinvim() {
-		mkdir -p ~/.cache/dein
-		(
-			cd ~/.cache/dein
-			curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-            # TODO: debug
-            pwd
-            ls -la
-			sh ./install.sh ~/.cache/dein
-		)
+        mkdir -p ~/.cache/dein
+        (
+            cd ~/.cache/dein
+            curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+            sh ./installer.sh ~/.cache/dein
+        )
 	}
 
     depend "install" "brew"
