778115f090b7c2ff76fb0b2be9d714ab4daa5626
humangas <humangas.net@gmail.com>
2017-11-23 21:01:29 +0900

    Added gems

---
+++ roles/vimgolf/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -u
+##############################################################################################
+# Usage: source from roles/setup.sh
+# Implementation of the following functions is required.
+# - is_installed, version, config, install, upgrade, config
+# The following functions can be used.
+# - log, depend
+# The following environment variables can be used.
+# - SETUP_CURRENT_ROLE_NAME, SETUP_CURRENT_ROLE_DIR_PATH
+##############################################################################################
+is_installed() {
+    type "$SETUP_CURRENT_ROLE_NAME" > /dev/null 2>&1; return $?
+}
+
+version() {
+    gem list --local "$SETUP_CURRENT_ROLE_NAME" | tail -n 1
+}
+
+config() {
+    return
+}
+
+install() {
+    depend "install" "ruby"
+    (
+        export PATH="/usr/local/bin:$PATH"
+        gem install "$SETUP_CURRENT_ROLE_NAME"
+    )
+    config
+}
+
+upgrade() {
+    install
+}
