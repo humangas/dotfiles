c5475db883bd32305534d817a00ab11e6283f000
humangas <humangas.net@gmail.com>
2017-11-29 20:26:22 +0900

    Updated usage

--- roles/setup.sh
+++ roles/setup.sh
@@ -41,6 +41,10 @@ Examples:
     $(basename $0) tags --tags GNU_commands,Quicklook ag
     $(basename $0) tag-add GNU_commands grep coreutils
 
+Convenient usage:
+    # List only roles that contain files
+    $ setup list | awk '\$10!=NULL{print \$1" "\$10}' | column -t
+
 EOS
 exit 1
 }
@@ -232,7 +236,7 @@ list() {
                     | sed "s@$SETUP_CURRENT_ROLE_DIR_PATH/$SETUP_TAGS_PREFIX@@" \
                     | paste -s -d '|' -)
         _files=$(find $SETUP_CURRENT_ROLE_DIR_PATH/ -type f \
-                    | /usr/bin/egrep -v "_template|setup\.sh|README\.md|tag\..*" \
+                    | /usr/bin/egrep -v "_template|disabled|setup\.sh|README\.md|tag\..*" \
                     | sed "s@$SETUP_CURRENT_ROLE_DIR_PATH/@@" \
                     | paste -s -d '|' -)
 
