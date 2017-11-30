11bd8e0911372271c59fa9ba76e5e692c5a44c0b
humangas <humangas.net@gmail.com>
2017-12-01 01:05:09 +0900

    Updated tmux config function

--- roles/tmux/setup.sh
+++ roles/tmux/setup.sh
@@ -21,8 +21,9 @@ version() {
 }
 
 config() {
-    depend "config" "go"
+    cp -fr "$SETUP_CURRENT_ROLE_DIR_PATH/.zsh.d" "$HOME/"
     cp "$SETUP_CURRENT_ROLE_DIR_PATH/.tmux.conf" "$HOME/"
+    mkdir -p "$HOME/bin"
     chmod u+x "$SETUP_CURRENT_ROLE_DIR_PATH/bin/"*
     cp -p "$SETUP_CURRENT_ROLE_DIR_PATH/bin/"* "$HOME/bin/"
 }
