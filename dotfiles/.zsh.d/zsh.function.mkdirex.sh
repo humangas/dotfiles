bd237ed3a4353df9cd4b6439218626e178d2bc94
humangas <humangas.net@gmail.com>
2018-05-21 06:45:11 +0000

    Updated dotfiles by Travis CI (BUILD 32:381543974) [skip ci]

--- dotfiles/.zsh.d/zsh.function.mkdirex.sh
+++ dotfiles/.zsh.d/zsh.function.mkdirex.sh
@@ -10,13 +10,13 @@ function mkdirex() {
                 echo 'Usage: mkdir -w DIRNAME'
                 return 1
             fi
-            mkdir -p "$GOPATH/src/work/_/$dirname"
+            mkdir -p "$GOPATH/src/work/$dirname"
             cd $_
             git init
             ;;
         --help)
             local _help=$(/usr/local/opt/coreutils/libexec/gnubin/mkdir --help)
-            echo $_help | sed -e '5i \　-w  DIRNAME   　  creating DIRNAME directory under $GOPATH/src/work/_/ and move it.'
+            echo $_help | sed -e '5i \　-w  DIRNAME   　  creating DIRNAME directory under $GOPATH/src/work/ and move it.'
             ;;
         *)
             /usr/local/opt/coreutils/libexec/gnubin/mkdir $@
