99c48842240c6db74edd27fa485455accc9afe88
humangas <humangas.net@gmail.com>
2020-04-26 16:26:57 +0900

    Modified: brew install url

--- roles/brew/setup.sh
+++ roles/brew/setup.sh
@@ -21,7 +21,7 @@ config() {
 }
 
 install() {
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     config
 }
 
