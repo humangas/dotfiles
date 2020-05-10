dacf7bf7a9615d4dda33fe30e453c28e351b8cd9
humangas <humangas.net@gmail.com>
2020-05-10 19:40:30 +0900

    Modified(roles/zsh): externalize colorscheme function

--- roles/zsh/main.sh
+++ roles/zsh/main.sh
@@ -17,23 +17,23 @@ _config() {
     cp -f .zshrc "$HOME/"
 }
 
+_colorscheme() {
+    local _filename="dircolors.256dark"
+    local filename="Solarized_$_filename"
+    local colorscheme_path="$HOME/.config/colorscheme"
+
+    log INFO "Setting color scheme for zsh..."
+    curl -sLO "https://raw.githubusercontent.com/seebi/dircolors-solarized/master/$_filename"
+    mv "$_filename" "$filename" 
+    mkdir -p "$colorscheme_path"
+    mv "$filename" "$colorscheme_path/$filename"
+}
+
 version() {
     basename "$(readlink /usr/local/opt/zsh)"
 }
 
 install() {
-    _colorscheme() {
-        local _filename="dircolors.256dark"
-        local filename="Solarized_$_filename"
-        local colorscheme_path="$HOME/.config/colorscheme"
-
-        log "INFO" "Setting color scheme for zsh..."
-        curl -sLO "https://raw.githubusercontent.com/seebi/dircolors-solarized/master/$_filename"
-        mv "$_filename" "$filename" 
-        mkdir -p "$colorscheme_path"
-        mv "$filename" "$colorscheme_path/$filename"
-    }
-
     _installed || {
         depend install brew
         brew install zsh
