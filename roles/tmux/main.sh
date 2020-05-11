6aaa9fc79b7f518fea8f64762f08be011f6a6f5b
humangas <humangas.net@gmail.com>
2020-05-11 19:43:23 +0900

    Remove header comment, Add link template/README.md

--- roles/tmux/main.sh
+++ roles/tmux/main.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -u
-##############################################################################################
-# Usage: source from roles/setup.sh
-# Implementation of the following functions is required.
-# - is_installed, version, config, install, upgrade, config
-# The following functions can be used.
-# - log, depend
-# The following environment variables can be used.
-# - SETUP_CURRENT_ROLE_NAME, SETUP_CURRENT_ROLE_DIR_PATH
-##############################################################################################
+# See: ../_templates/README.md
+
+
 _installed() {
     brew list tmux > /dev/null 2>&1; return $?
 }
