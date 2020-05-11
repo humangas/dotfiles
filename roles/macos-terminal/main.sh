26d82f33e387876b0b9a283c30567d36363be073
humangas <humangas.net@gmail.com>
2020-05-11 20:31:11 +0900

    Modified(macos-terminal): refactoring

--- roles/macos-terminal/main.sh
+++ roles/macos-terminal/main.sh
@@ -2,33 +2,29 @@
 # See: ../_templates/README.md
 
 
+_colorscheme() {
+    local profile='Solarized_Dark'
+    local filename="$(echo "$profile" | sed -e 's/_/%20/g').terminal"
+    local current_profile="$(defaults read com.apple.terminal 'Default Window Settings')"
+    local colorscheme_path="$HOME/.config/colorscheme"
+
+    if [ "${current_profile}" != "${profile}" ]; then
+        log INFO "Setting color scheme for Terminal..."
+        mkdir -p "$colorscheme_path"
+        curl -sL -o "$colorscheme_path/$profile.terminal" "https://raw.githubusercontent.com/tomislav/osx-terminal.app-colors-solarized/master/$filename"
+        open "$colorscheme_path/$profile.terminal"
+        defaults write com.apple.Terminal "Default Window Settings" -string "$profile"
+        defaults write com.apple.Terminal "Startup Window Settings" -string "$profile"
+    fi
+
+    defaults import com.apple.Terminal "$HOME/Library/Preferences/com.apple.Terminal.plist"
+}
+
 version() { 
     echo "None"
 }
 
 install() {
-    _colorscheme() {(
-        cd "$SETUP_CURRENT_ROLE_DIR_PATH"
-
-        local profile='Solarized_Dark'
-        local filename="$(echo "$profile" | sed -e 's/_/%20/g').terminal"
-        local current_profile="$(defaults read com.apple.terminal 'Default Window Settings')"
-        local colorscheme_path="$HOME/.config/colorscheme"
-
-        if [ "${current_profile}" != "${profile}" ]; then
-            log "INFO" "Setting color scheme for Terminal..."
-            curl -sLO "https://raw.githubusercontent.com/tomislav/osx-terminal.app-colors-solarized/master/$filename"
-            mv "$filename" "$profile.terminal"
-            mkdir -p "$colorscheme_path"
-            mv "$profile.terminal" "$colorscheme_path"
-            open "$colorscheme_path/$profile.terminal"
-            defaults write com.apple.Terminal "Default Window Settings" -string "$profile"
-            defaults write com.apple.Terminal "Startup Window Settings" -string "$profile"
-        fi
-
-        defaults import com.apple.Terminal "$HOME/Library/Preferences/com.apple.Terminal.plist"
-    )}
-
     _colorscheme
 
     # Only use UTF-8 in Terminal.app
